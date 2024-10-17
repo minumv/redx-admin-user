c5b5483c49c32959a43bca11cb4eb67d671fc676
Minu M V <minumahi2017@gmail.com>
2024-10-17 10:54:40 +0530

    Create header components

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,28 @@
+
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <div className="bg-slate-200">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3 ">
+        <div>
+          <h1 className="font-bold capitalize">redx-admin-user</h1>
+        </div>
+        <ul className="flex gap-3">
+            <Link to="/">
+                <li>Home</li>
+            </Link>
+            <Link to="/about">
+                <li>About</li>
+            </Link> 
+            <Link to="/about">
+                <li>Sign in</li>
+            </Link>             
+                 
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Header
